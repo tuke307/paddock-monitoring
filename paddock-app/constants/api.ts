db1016e64a2f98c571fd276624262e4994369c3b
Tony <tonymeissner70@gmail.com>
2024-11-13 14:54:45 +0100

    fix: Update API_URL to use the correct production endpoint

--- paddock-app/constants/api.ts
+++ paddock-app/constants/api.ts
@@ -1,1 +1,1 @@
-export const API_URL = 'https://api-paddockmonitoring-prod-ewe8e3g4h7egh7gq.westeurope-01.azurewebsites.net:443/api/v1/production';
+export const API_URL = 'https://api-paddockmonitoring-prod.azurewebsites.net:443/api/v1/production';
